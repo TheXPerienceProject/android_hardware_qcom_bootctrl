af984301c1eb0fb9253a7fe0eb439d2eac10404f
Ameya Thakur <ameyat@codeaurora.org>
2016-09-23 14:50:14 -0700

    bootcontrol: Only compile when the A/B feature is enabled
    We now only compile the HAL when the A/B feature flag is set.
    
    Change-Id: I822fe4579de86e8785dd5708e4a063311a61d060
    Signed-off-by: klozz <klozz@vivaldi.net>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,7 @@
 ifeq ($(AB_OTA_UPDATER),true)
+# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
+ifneq ($(BOARD_IS_AUTOMOTIVE),true)
+ifneq ($(filter msm8996 msm8998 sdm845,$(TARGET_BOARD_PLATFORM)),)
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/gpt-utils/inc
@@ -11,4 +14,6 @@ LOCAL_MODULE := bootctrl.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_OWNER := qti
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
+endif
+endif
 endif
